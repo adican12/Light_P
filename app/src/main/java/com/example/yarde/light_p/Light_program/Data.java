d12f9f22825250e7a696731959dcfbdf7dfb5bea
Adican12 <adican12@gmail.com>
2018-06-04 00:55:48 +0300

    firebase not ready getPower - need to check

--- app/src/main/java/com/example/yarde/light_p/Light_program/Data.java
+++ app/src/main/java/com/example/yarde/light_p/Light_program/Data.java
@@ -1,21 +1,63 @@
 package com.example.yarde.light_p.Light_program;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 /**
  * Created by yarde on 03/06/2018.
  */
-
+@IgnoreExtraProperties
 public class Data {
 
+    private float lightSensor;
+    private double longitude;
+    private double altitude;
 
+    public Data(){}
 
-    public int lightSensor;
-//    public String locationSensor;
-//    public String Date;
+    public Data(float _lightSensor,double _longitude,double _altitude) {
+        this.lightSensor = _lightSensor;
+        this.longitude =_longitude;
+        this.altitude = _altitude;
+    }
 
-    public Data(int lightSensor) {
+    public Data(float lightSensor) {
+        this.lightSensor = lightSensor;
+        this.longitude =0;
+        this.altitude = 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "lightSensor=" + lightSensor +
+                ", longitude=" + longitude +
+                ", altitude=" + altitude +
+                '}';
+    }
+
+    public float getLightSensor() {
+        return lightSensor;
+    }
+
+    public void setLightSensor(float lightSensor) {
         this.lightSensor = lightSensor;
     }
 
+    public double getLongitude() {
+        return longitude;
+    }
 
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
 }
 
--- app/src/main/java/com/example/yarde/light_p/SensorActivity.java
+++ app/src/main/java/com/example/yarde/light_p/SensorActivity.java
@@ -1,12 +1,18 @@
 package com.example.yarde.light_p;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,23 +21,26 @@ import com.example.yarde.light_p.Light_program.Data;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-//import com.example.yarde.light_p.helpers.MQTTHelper;
-
-//import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-//import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
-//import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class SensorActivity extends Activity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     Sensor sensor;
     Sensor sensorGPS;
+    float mLightSensor;
+
+    LocationManager locationManager;
+    Location loc;
+    double longitude, altitude ;
+
     TextView textView;  //text view for result
     TextView dataReceived;
-    //FirebaseDatabase mRef;
-    int mLightSensor ;
 
-    //MQTTHelper mqttHelper;
+
+    private DatabaseReference mDatabase;
+    int i = 0;
+
+
     private Button Report_data;
 
 
@@ -40,52 +49,17 @@ public class SensorActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_sensor);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);  //accses to senspr manger
+//        sensorGPS = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);   // choose sensor
         sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);   // choose sensor
-        sensorGPS =mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);   // choose sensor
 
-
-
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
 
         textView = (TextView) findViewById(R.id.textView);  // show deatils
         dataReceived = (TextView) findViewById(R.id.dataReceived);
 
-        //mRef = new FirebaseDatabase("https://lightp-ef920.firebaseio.com/");
         Report_data = (Button) findViewById(R.id.repord_data);
 
-  //      startMqtt();
     }
-
-
-
-//    private void startMqtt() {
-//        mqttHelper = new MQTTHelper(getApplicationContext());
-//        mqttHelper.setCallback(new MqttCallbackExtended() {
-//            @Override
-//            public void connectComplete(boolean b, String s) {
-//
-//            }
-//
-//            @Override
-//            public void connectionLost(Throwable throwable) {
-//
-//            }
-//
-//            @Override
-//            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-//                Log.w("Debug", mqttMessage.toString());
-//                dataReceived.setText(mqttMessage.toString());
-//            }
-//
-//            @Override
-//            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-//
-//            }
-//        });
-//
-
-//    }
-
-
 
     @Override
     public void onSensorChanged(SensorEvent event) {  //check changed
@@ -93,9 +67,9 @@ public class SensorActivity extends Activity implements SensorEventListener {
             textView.setText("" + event.values[0]);  //update text view
 
         }
-        if (event.sensor.getType() == sensorGPS.TYPE_MAGNETIC_FIELD) {
-            dataReceived.setText("" + event.values[0]);
-        }
+//        if (event.sensor.getType() == sensorGPS.TYPE_MAGNETIC_FIELD) {
+//            dataReceived.setText("" + event.values[0]);
+//        }
 
 
     }
@@ -112,53 +86,47 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
 
     protected void onResume() {
-//        // Register a listener for the sensor.
+        // Register a listener for the sensor.
         super.onResume();
         mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, sensorGPS, SensorManager.SENSOR_DELAY_NORMAL);
+//        mSensorManager.registerListener(this, sensorGPS, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
-    //
-//    @Override
+    @Override
     protected void onPause() {
-//        // Be sure to unregister the sensor when the activity pauses.
+        // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
 
-//
-//    private void writeNewUser(String lightSensor) {
-//        Data data = new Data(lightSensor);
-//        DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference();//work version
-//        mdatabase.child("data").setValue(data);
-//
-//    }
-//
-//
 
-    public void onClick(View view){
-        int i = 0;
-        FirebaseDatabase database = FirebaseDatabase.getInstance();//work version
-        //DatabaseReference myRef = database.getReference("message");//work version
-        DatabaseReference myRef = database.getReference("root");//check version
-        mLightSensor = Sensor.TYPE_LIGHT;
-        Data data = new Data(mLightSensor);
-        //DatabaseReference mdatabase = FirebaseDatabase.getInstance().getReference();//work version
-        //mdatabase.child("data").setValue(data);
-//
-        myRef.getRoot().child("data : "+i ).setValue(data);
+    public void onClick(View view) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //
+        // require premission to check location services
+        //
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = loc.getLongitude();
+        altitude = loc.getLatitude();
 
-        ++i;
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);   // choose sensor
+        mLightSensor=sensor.getPower();
 
-//        writeNewUser();
-
-        //myRef.setValue(textView.getText());
-        //myRef1.setValue(textView.getText());
+        Data _data = new Data(mLightSensor);
+//        Data _data = new Data(mLightSensor,longitude,altitude );
+        mDatabase.child("Data").child(String.valueOf(i++)).setValue(_data);
 
     }
-
-
-
 }
 
